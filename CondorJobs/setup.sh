925f0bee44478e31521ef36fb8eefc17b5f4b6bc
bfontana <bruno.alves@cern.ch>
2020-03-04 16:45:44 +0100

    ntuple IDs extraction

--- CondorJobs/setup.sh
+++ CondorJobs/setup.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+
+#Clean jobs output files
 echo "Are you sure you want to delete the contents of the out/ and log/ folders? [y/n]"
 while true; do
     read in
@@ -12,3 +14,6 @@ while true; do
 	echo "Please provide a valid answer. [y/n]"
     fi
 done;
+
+#Generate file with the IDs of the Ntuples to be processed
+ls -l ../ntuples/ | awk '{print substr($9,8)}' | awk '{print substr($1,1,length($1)-5)}' > ntuple_ids.txt
