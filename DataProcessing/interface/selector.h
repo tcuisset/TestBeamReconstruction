c2ca7779216a8ea38d8e5df99525669e05ee12c3
bfontana <bruno.alves@cern.ch>
2020-03-20 16:41:14 +0100

    printing method added

--- DataProcessing/interface/selector.h
+++ DataProcessing/interface/selector.h
@@ -1,22 +1,30 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include "TTree.h"
 #include "ROOT/RDataFrame.hxx"
+#include "ROOT/RDF/InterfaceUtils.hxx"
 
 class Selector {
  public:
-  Selector(const std::string&, const std::string&, std::optional<std::string> in_tree_name = std::nullopt, std::optional<std::string> out_tree_name = std::nullopt);
+  Selector(const std::string&, std::optional<std::string> out_file_path = std::nullopt, std::optional<std::string> in_tree_name = std::nullopt, std::optional<std::string> out_tree_name = std::nullopt);
   ~Selector();
   void select_relevant_branches();
+  void print_relevant_branches(const int&, std::optional<std::string> filename = std::nullopt);
 
  private:
+  //methods
   int sanity_checks(const std::string&);
+  
+  //data
+  static const int ncpus_ = 4;
+  const ROOT::Detail::RDF::ColumnNames_t cols_ = {"event", "run", "NRechits", "rechit_detid", "rechit_x", "rechit_y", "rechit_z", "rechit_layer", "rechit_iu", "rechit_iv", "rechit_iU", "rechit_iV", "rechit_type", "rechit_energy", "beamEnergy", "trueBeamEnergy", "pdgID"};
   struct indata {
     std::string file_path = "/eos/cms/store/group/dpg_hgcal/tb_hgcal/2018/cern_h2_october/offline_analysis/ntuples/v16/ntuple_1000.root";
     std::string tree_name = "rechitntupler/hits";
   }indata_;
   struct outdata {
-    std::string file_path = "/eos/user/b/bfontana/TestBeamReconstruction/output.txt";
+    std::string file_path = "/eos/user/b/bfontana/TestBeamReconstruction/default_output.txt";
     std::string tree_name = "relevant_branches";
   }outdata_;
 };
--- DataProcessing/src/selector.cc
+++ DataProcessing/src/selector.cc
@@ -1,12 +1,15 @@
 #include "UserCode/DataProcessing/interface/selector.h"
 
-Selector::Selector(const std::string& in_file_path, const std::string& out_file_path,
+Selector::Selector(const std::string& in_file_path, std::optional<std::string> out_file_path,
 		   std::optional<std::string> in_tree_name, std::optional<std::string> out_tree_name)
 {
   sanity_checks(in_file_path);
-  sanity_checks(out_file_path);
   this->indata_.file_path = in_file_path;
-  this->outdata_.file_path = out_file_path;
+  if(in_tree_name != std::nullopt)
+    {
+      this->outdata_.file_path = out_file_path.value();
+      sanity_checks(this->outdata_.file_path);
+    }
   if(in_tree_name != std::nullopt)
     this->indata_.tree_name = in_tree_name.value();
   if(out_tree_name != std::nullopt)
@@ -19,12 +22,30 @@ Selector::~Selector()
 
 void Selector::select_relevant_branches()
 {
-  const int ncpus = 4;
-  ROOT::EnableImplicitMT( ncpus );
+  if(this->outdata_.file_path == "/eos/user/b/bfontana/TestBeamReconstruction/default_output.txt")
+    throw std::invalid_argument("In order to perform TTree skimming you must specify an output file path.");
+  //enable parallelism
+  ROOT::EnableImplicitMT( ncpus_ );
+  //define dataframe that owns the TTree
   ROOT::RDataFrame d(this->indata_.tree_name.c_str(), this->indata_.file_path.c_str());
-  std::cout << this->indata_.tree_name.c_str() << ", " << this->indata_.file_path.c_str() << std::endl;
-  std::cout << this->outdata_.tree_name.c_str() << ", " << this->outdata_.file_path.c_str() << std::endl;
-  d.Snapshot(this->outdata_.tree_name.c_str(), this->outdata_.file_path.c_str(), {"rechit_x", "rechit_y", "rechit_z", "rechit_layer", "rechit_iu", "rechit_iv", "rechit_iU", "rechit_iV", "rechit_type", "rechit_energy", "beamEnergy", "pdgID"});
+  //store the contents of the TTree according to the specified columns
+  d.Snapshot(this->outdata_.tree_name.c_str(), this->outdata_.file_path.c_str(), cols_);
+};
+
+void Selector::print_relevant_branches(const int& nrows=5, std::optional<std::string> filename)
+{
+  //define dataframe that owns the TTree
+  ROOT::RDataFrame d(this->indata_.tree_name.c_str(), this->indata_.file_path.c_str());
+  //prints TTree content according to the specified columns; a multi-threaded version is not supported
+  auto display = d.Display(cols_, nrows);
+  //write contents to file
+  std::ofstream f;
+  if(filename != std::nullopt)
+    f.open(filename.value());
+  else
+    f.open("data.txt");
+  f << display->AsString();
+  f.close();
 };
 
 int Selector::sanity_checks(const std::string& fname) 
