533983744a7a4cdb2d5d86fa58eab98694a8a2ac
bfontana <bruno.alves@cern.ch>
2020-03-23 16:15:32 +0100

    add print_data

---
+++ DataProcessing/bin/print_data.cc
@@ -0,0 +1,11 @@
+#include "UserCode/DataProcessing/interface/selector.h"
+
+void print_tree_contents(const std::string& in_fname) {
+  Selector selector(in_fname);
+  selector.print_relevant_branches(1);
+}
+
+int main(int argc, char **argv) {
+  print_tree_contents(std::string(argv[1]));
+  return 0;
+}
