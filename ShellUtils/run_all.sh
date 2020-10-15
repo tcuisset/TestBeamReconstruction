d7cf539528038b019fdf59f3073648ee5e5bfca8
Bruno Alves <balves16@hotmail.com>
2020-10-15 11:25:48 +0200

    imporve shell scripts

--- ShellUtils/run_all.sh
+++ ShellUtils/run_all.sh
@@ -9,12 +9,14 @@ declare -a VARS=( "dx"
 		  "dy"
 		  "dxdy"
 		  "posx_posy" )
+declare -a BEAM_ENERGY=( 20 30 50 80 100 120 150 200 250 300 )
 USE_SAVED_DATA=0
+BEAM_ENERGY=50
 
 ######################################
 ##Agument parsing#####################
 ######################################
-ARGS=`getopt -o "" -l ",use_saved_data:,var:" -n "getopts_${0}" -- "$@"`
+ARGS=`getopt -o "" -l ",chosen_energy:,use_saved_data:,var:" -n "getopts_${0}" -- "$@"`
 
 #Bad arguments
 if [ $? -ne 0 ];
@@ -43,6 +45,18 @@ while true; do
 		echo "Use saved data.";
 	    fi
 	    shift 2;;
+
+	--chosen_energy)
+	    if [ -n "$2" ]; then
+		if [[ ! " ${BEAM_ENERGY[@]} " =~ " ${2} " ]]; then
+		    echo "Beam energy ${2} is not part of the analysis."
+		    exit 1;
+		else
+		    BEAM_ENERGY="${2}";
+		    echo "variable to consider: ${BEAM_ENERGY}";
+		fi
+	    fi
+	    shift 2;;
 	
 	--)
 	    shift
@@ -54,10 +68,11 @@ done
 ##Run the code########################
 ######################################
 for i in $(seq 0 $(expr "${#TAGS[@]}" - 1)); do
+    COMMAND="python DataProcessing/python/cluster_dep.py --datatype data --showertype em --${VAR} --chosen_energy ${BEAM_ENERGY} --tag ${TAGS[${i}]}"
     if [ ${USE_SAVED_DATA} -eq 1 ]; then
-	python DataProcessing/python/cluster_dep.py --datatype data --showertype em --${VAR} --tag ${TAGS[${i}]} --use_saved_data &
+	${COMMAND} --use_saved_data &
     else
-	python DataProcessing/python/cluster_dep.py --datatype data --showertype em --${VAR} --tag ${TAGS[${i}]} &
+	${COMMAND} &
     fi
     pids[${i}]=$!
 done
@@ -68,3 +83,4 @@ done
 for pid in ${pids[*]}; do
     wait $pid
 done
+echo "All jobs finished."
--- ShellUtils/write_and_submit.sh
+++ ShellUtils/write_and_submit.sh
@@ -1,17 +1,18 @@
-write_dag --datatype data --showertype em --w0 2.3 --dpos 1.3 --tag W2p3_dpos1p3 --last_step_only;
-condor_submit_dag CondorJobs/clue_data_em_W2p3_dpos1p3_analysis_only.dag;
+declare -a TAGS=( "W5p0_dpos999" )
+#                  "W2p9_dpos1p3"
+#		  "W4p0_dpos1p3"
+#		  "W5p0_dpos1p3"
+#                 "W2p9_dpos3p4"
+#                  "W2p9_dpos999" )
 
-write_dag --datatype data --showertype em --w0 2.9 --dpos 1.3 --tag W2p9_dpos1p3 --last_step_only;
-condor_submit_dag CondorJobs/clue_data_em_W2p9_dpos1p3_analysis_only.dag;
+for i in `seq 0 $(expr "${#TAGS[@]}" - 1)`; do
+    W0=${TAGS[i]%_dpos*} #remove everything starting from '_dpos'
+    W0=${W0:1:${#W0}} #remove the initial 'W'
+    W0=`echo ${W0} | sed 's/p/./g'` #replace 'p' by '.'v
+    DPOS=${TAGS[i]##*_dpos} #remove everything before, and including, '_dpos'
+    DPOS=`echo ${DPOS} | sed 's/p/./g'` #replace 'p' by '.'
 
-write_dag --datatype data --showertype em --w0 4.0 --dpos 1.3 --tag W4p0_dpos1p3 --last_step_only;
-condor_submit_dag CondorJobs/clue_data_em_W4p0_dpos1p3_analysis_only.dag;
+    write_dag --datatype data --showertype em --w0 ${W0} --dpos ${DPOS} --tag ${TAGS[i]} --last_step_only;
+    condor_submit_dag CondorJobs/clue_data_em_"${TAGS[i]}"_analysis_only.dag;
 
-write_dag --datatype data --showertype em --w0 5.0 --dpos 1.3 --tag W5p0_dpos1p3 --last_step_only;
-condor_submit_dag CondorJobs/clue_data_em_W5p0_dpos1p3_analysis_only.dag;
-
-write_dag --datatype data --showertype em --w0 2.9 --dpos 3.4 --tag W2p9_dpos3p4 --last_step_only;
-condor_submit_dag CondorJobs/clue_data_em_W2p9_dpos3p4_analysis_only.dag;
-
-write_dag --datatype data --showertype em --w0 2.9 --dpos 999.0 --tag W2p9_dpos999 --last_step_only;
-condor_submit_dag CondorJobs/clue_data_em_W2p9_dpos999_analysis_only.dag;
+done
