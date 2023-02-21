tcuisset/TestBeamReconstruction

commit f6d1b24da3b72dc5e5271d791a9ddb4bfd879b94

Fix compilation error due to RDataFrame ImplicitMT

DataProcessing/src/selector.cc
@@ -241,8 +241,6 @@ void Selector::select_relevant_branches()
   t_had1->AddFriend(t_had2, "myFriend");
   d = new ROOT::RDataFrame(*t_had1);
 
-  ROOT::EnableImplicitMT( ncpus_ ); //enable parallelism
-  ROOT::Detail::RDF::ColumnNames_t clean_cols = {"rechit_energy", "rechit_layer", "rechit_chip", "rechit_channel", "rechit_module", "rechit_amplitudeHigh", "rechit_noise_flag", "st"};
   ROOT::Detail::RDF::ColumnNames_t clean_cols_detid = clean_cols; clean_cols_detid.insert( clean_cols_detid.begin(), "rechit_detid");
   ROOT::Detail::RDF::ColumnNames_t clean_cols_x = clean_cols; clean_cols_x.insert( clean_cols_x.begin(), "rechit_x");
   ROOT::Detail::RDF::ColumnNames_t clean_cols_y = clean_cols; clean_cols_y.insert( clean_cols_y.begin(), "rechit_y");

DataProcessing/bin/analyze_data.cc
@@ -45,6 +45,9 @@ int main(int argc, char **argv) {
     st = SHOWERTYPE::EM;
   else if( showertype == "had" )
     st = SHOWERTYPE::HAD;
+
+  ROOT::EnableImplicitMT(4); //enable parallelism
+  
   analysis_CLUE(in_fname, out_fname, out_fname_layer_dependent, out_fname_cluster_dependent, in_tname, st, W0, dpos);
   return 0;
 }

DataProcessing/bin/process_data.cc
@@ -7,6 +7,8 @@ int main(int argc, char **argv) {
   std::string showertype = std::string(argv[4]);
   int beam_energy = std::stoi( std::string(argv[5]) );
 
+  ROOT::EnableImplicitMT(4); //enable parallelism
+
   Selector selector(input_file, output_file, datatype, showertype, beam_energy);
   selector.select_relevant_branches();
   

DataProcessing/interface/analyzer.h
@@ -50,7 +50,6 @@ class Analyzer {
   
   //data
   size_t nfiles_;
-  static const int ncpus_ = 4;
   unsigned lmax=0;
   float dc_, kappa_, ecut_;
   SHOWERTYPE st_;

DataProcessing/interface/selector.h
@@ -33,7 +33,6 @@ class Selector {
   SHOWERTYPE showertype;
   DATATYPE datatype;
   int beam_energy;
-  static const int ncpus_ = 4;
   mapT noise_map_;
   std::vector< std::pair<float,float> > shifts_map_;
   

DataProcessing/src/analyzer.cc
@@ -162,19 +162,18 @@ std::pair<unsigned int, float> Analyzer::_readTree( const std::string& infile,
 			std::vector< std::vector<unsigned int> >& layer, std::vector< std::vector<float> >& weight, 
 	                std::vector< std::vector<unsigned int> >& rechits_id,
 		        std::vector< std::vector<float> >& impactX, std::vector< std::vector<float> >& impactY) {
-  //enable parallel execution
-  ROOT::EnableImplicitMT( ncpus_ );
   //creates RDataFrame object
   std::string intree = "relevant_branches";
   ROOT::RDataFrame d( intree.c_str(), infile.c_str() );
+  unsigned int ncpus = ROOT::GetThreadPoolSize();
   //declare data vectors per event to be separately filled by independent cpu threads. dimension: (ncpus, nentries)
-  std::array< std::vector< std::vector<float> >, ncpus_ > x_split;
-  std::array< std::vector< std::vector<float> >, ncpus_ > y_split;
-  std::array< std::vector< std::vector<unsigned int> >, ncpus_ > layer_split;
-  std::array< std::vector< std::vector<float> >, ncpus_ > weight_split;
-  std::array< std::vector< std::vector<unsigned int> >, ncpus_ > rechits_id_split;
-  std::array< std::vector< std::vector<float> >, ncpus_ > impactX_split;
-  std::array< std::vector< std::vector<float> >, ncpus_ > impactY_split;
+  std::vector< std::vector< std::vector<float> > > x_split(ncpus);
+  std::vector< std::vector< std::vector<float> > > y_split(ncpus);
+  std::vector< std::vector< std::vector<unsigned int> > > layer_split(ncpus);
+  std::vector< std::vector< std::vector<float> > > weight_split(ncpus);
+  std::vector< std::vector< std::vector<unsigned int> > > rechits_id_split(ncpus);
+  std::vector< std::vector< std::vector<float> > > impactX_split(ncpus);
+  std::vector< std::vector< std::vector<float> > > impactY_split(ncpus);
 
   float beam_energy = 0;
   //lambda function passed to RDataFrame.ForeachSlot(); the first parameters gives the thread number contained in [0;ncpus[
@@ -201,7 +200,7 @@ std::pair<unsigned int, float> Analyzer::_readTree( const std::string& infile,
 
   //calculate number of events taking into account that ncpus is just a hint to EnableImplicitMT
   std::vector<unsigned int> nevents_v;
-  for(unsigned int iThread=0; iThread<ncpus_; ++iThread) {
+  for(unsigned int iThread=0; iThread<ncpus; ++iThread) {
     nevents_v.push_back( x_split[iThread].size() ); //assumes all the sizes are the same
   }
   unsigned int nevents = std::accumulate(nevents_v.begin(), nevents_v.end(), 0);
@@ -257,8 +256,6 @@ bool Analyzer::ecut_selection(const float& energy, const unsigned int& layer)
 void Analyzer::sum_energy(const bool& with_ecut)
 {
   std::mutex mut; //anonymous function to pass to RDataFrame.ForEach()
-
-  ROOT::EnableImplicitMT( ncpus_ ); //enable parallelism
 
   for(unsigned int i=0; i<nfiles_; ++i)
     {

commit 5c8816c732638089e839e028b4c1c636526e1f07

Fix: Commit f6d1b24 inadvertently removed one extra line of code

DataProcessing/src/selector.cc
@@ -241,6 +241,7 @@ void Selector::select_relevant_branches()
   t_had1->AddFriend(t_had2, "myFriend");
   d = new ROOT::RDataFrame(*t_had1);
 
+  ROOT::Detail::RDF::ColumnNames_t clean_cols = {"rechit_energy", "rechit_layer", "rechit_chip", "rechit_channel", "rechit_module", "rechit_amplitudeHigh", "rechit_noise_flag", "st"};
   ROOT::Detail::RDF::ColumnNames_t clean_cols_detid = clean_cols; clean_cols_detid.insert( clean_cols_detid.begin(), "rechit_detid");
   ROOT::Detail::RDF::ColumnNames_t clean_cols_x = clean_cols; clean_cols_x.insert( clean_cols_x.begin(), "rechit_x");
   ROOT::Detail::RDF::ColumnNames_t clean_cols_y = clean_cols; clean_cols_y.insert( clean_cols_y.begin(), "rechit_y");